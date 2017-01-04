6b264f3099b2551c5d13c9e52847707490fec565
istiophorus <rskotak@gmail.com>
2017-01-04 23:41:32 +0100

    working on multi-graoh

--- RFirstproject/RFirstproject/script.R
+++ RFirstproject/RFirstproject/script.R
@@ -12,11 +12,11 @@ getIngBaseData = function(symbol, period) {
     fromJSON(resource)
 }
 
-adidasWeek <- getIngBaseData("adidas", "week")
-adidasCertIntraday <- getIngCertData("PLINGNV14787", "intraday")
-pgeCertIntraday <- getIngCertData("PLINGNV16725", "intraday")
-pgnCertIntraday <- getIngCertData("PLINGNV04713", "intraday")
-bundCertIntraday <- getIngCertData("PLINGNV00497", "intraday")
+#adidasWeek <- getIngBaseData("adidas", "week")
+#adidasCertIntraday <- getIngCertData("PLINGNV14787", "intraday")
+#pgeCertIntraday <- getIngCertData("PLINGNV16725", "intraday")
+#pgnCertIntraday <- getIngCertData("PLINGNV04713", "intraday")
+#bundCertIntraday <- getIngCertData("PLINGNV00497", "intraday")
 
 drawCertGraph <- function(graphTitle, inputData) {
     certAsk <- inputData$AskQuotes
@@ -28,6 +28,11 @@ drawCertGraph <- function(graphTitle, inputData) {
     title(main = graphTitle)
 }
 
+drawCertGraphOnScreen <- function(screenIndex, graphTitle, inputData) {
+    screen(screenIndex)
+    drawCertGraph(graphTitle, inputData)
+}
+
 initializeData <- function() {
     titles <- c("adidas", "pgn", "pge", "bund")
     certs <- c("PLINGNV14787", "PLINGNV04713", "PLINGNV16725", "PLINGNV00497")
@@ -36,7 +41,7 @@ initializeData <- function() {
     certItems
 }
 
-drawAllGraphs <- function(itemsList, period) {
+getAllData <- function(itemsList, period) {
     results <- list()
 
     for (i in 1:nrow(itemsList)) {
@@ -50,8 +55,37 @@ drawAllGraphs <- function(itemsList, period) {
     results
 }
 
-d <- initializeData()
-dx <- drawAllGraphs(d, "intraday")
+itemsList <- initializeData()
+allData <- getAllData(d, "intraday")
+
+drawAllGraphs <- function(itemsList, allData) {
+    par(bg = "white") # erase.screen() will appear not to work
+                  # if the background color is transparent 
+                  # (as it is by default on most devices).
+
+    # prepare screens
+
+    split.screen(c(2, 1))
+    split.screen(c(1, 3), screen = 1)
+    split.screen(c(1, 3), screen = 2) # now split the bottom half into 3
+
+    screenIndex = 3
+
+    for (i in 1:nrow(itemsList)) {
+        print(i)
+        currentItem = itemsList[i,]
+        print(currentItem)
+        currentItemData <- allData[i,]
+
+        drawCertGraphOnScreen(screenIndex, currentItem$titles, currentItemData)
+    }
+}
+
+drawAllGraphs(itemsList, allData)
+
+
+
+
 
 #drawCertGraph("adidas", adidasCertIntraday)
 #drawCertGraph("pgn", pgnCertIntraday)
